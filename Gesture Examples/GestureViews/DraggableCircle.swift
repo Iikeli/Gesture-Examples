4b58558a01dfcb98c60ed080c3e9fc0d6188f507
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-03-25 13:45:14 +0200

    Replace depricated animation(_) with animation(_, value:) and fixed indentation.

--- Gesture Examples/GestureViews/DraggableCircle.swift
+++ Gesture Examples/GestureViews/DraggableCircle.swift
@@ -39,37 +39,36 @@ struct DraggableCircle: View {
     @State var viewState = CGSize.zero
 
     var body: some View {
-            let minimumLongPressDuration = 0.1
-            let longPressDrag = LongPressGesture(minimumDuration: minimumLongPressDuration)
-                .sequenced(before: DragGesture())
-                .updating($dragState) { value, state, transaction in
-                    switch value {
+        let minimumLongPressDuration = 0.1
+        let longPressDrag = LongPressGesture(minimumDuration: minimumLongPressDuration)
+            .sequenced(before: DragGesture())
+            .updating($dragState) { value, state, transaction in
+                switch value {
                     // Long press begins.
-                    case .first(true):
-                        state = .pressing
+                case .first(true):
+                    state = .pressing
                     // Long press confirmed, dragging may begin.
-                    case .second(true, let drag):
-                        state = .dragging(translation: drag?.translation ?? .zero)
+                case .second(true, let drag):
+                    state = .dragging(translation: drag?.translation ?? .zero)
                     // Dragging ended or the long press cancelled.
-                    default:
-                        state = .inactive
-                    }
+                default:
+                    state = .inactive
                 }
-                .onEnded { value in
-                    guard case .second(true, let drag?) = value else { return }
-                    self.viewState.width += drag.translation.width
-                    self.viewState.height += drag.translation.height
-                }
+            }
+            .onEnded { value in
+                guard case .second(true, let drag?) = value else { return }
+                self.viewState.width += drag.translation.width
+                self.viewState.height += drag.translation.height
+            }
 
-        return
-        ZStack {
+        return ZStack {
             Circle()
                 .foregroundColor(.mint)
                 .overlay(dragState.isDragging ? Circle().stroke(Color.white, lineWidth: 2) : nil)
                 .frame(width: 150, height: 150, alignment: .center)
-                .animation(nil)
+                .animation(nil, value: dragState.isActive)
                 .shadow(radius: dragState.isActive ? 8 : 0)
-                .animation(.linear(duration: minimumLongPressDuration))
+                .animation(.linear(duration: minimumLongPressDuration), value: dragState.isActive)
             Text("Drag me")
                 .font(.title)
         }
