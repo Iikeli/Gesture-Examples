ee696ec9e68832c0f7900e1aa53ddb0d8a89dab1
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-03-25 09:44:00 +0200

    Initial Commit

---
+++ Gesture Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gesture Examples
+//
+//  Created by Iiro Alhonen on 25.3.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gesture Examples/Gesture_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gesture_ExamplesApp.swift
+//  Gesture Examples
+//
+//  Created by Iiro Alhonen on 25.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct Gesture_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
